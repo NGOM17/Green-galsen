29e9bd8f62f7171080669e8361babe3d9791ec93
Baron-aw <Mamadoumactar.aw@unchk.edu.sn>
2026-06-27 18:16:21 +0000

    Structure de base
     Green Galsen

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html> 
+<html lang="fr"> 
+    <head> 
+        <meta charset="UTF-8"> 
+    <title>Green Galsen | Accueil</title> 
+</head> 
+    <body>
+         <h1>Bienvenue sur Green Galsen</h1>
+     <p>Plateforme de commande de produits agricoles locaux.</p> 
+    </body> 
+     </html>
